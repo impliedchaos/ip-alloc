4bbd468eda03e93b03b66a194efdf9e43f5970d1
impliedchaos <crap@dharmadevil.com>
2022-03-01 05:35:47 -0500

    Better whois country search

--- ripepreproc.pl
+++ ripepreproc.pl
@@ -6,6 +6,15 @@
 use strict;
 use warnings;
 
+open(IN,"<","countryInfo.txt") or die($!);
+my (%CO,%CC);
+while(<IN>) {
+   next if (/^\s*#/);
+   my @c = split(/\t/,$_);
+   $CO{$c[4]} = $c[0];
+   $CC{$c[5]} = $c[0];
+}
+close(IN);
 while(<>) {
    chomp;
    my @t = split(/\|/,$_);
@@ -13,6 +22,36 @@ while(<>) {
       my $w = `whois -h whois.ripe.net $t[3]`;
       if ($w =~ /^country:\s*([A-Z]{2,2})/m) {
          $t[1] = $1;
+      }
+      if ($t[1] eq 'EU') {
+         print STDERR "WHOIS for $t[3] still showing as EU.\n";
+         print STDERR "Attempting Country lookup from address lines.\n";
+         while ($w =~ /^address:\s*(.*)$/mg && $t[1] eq 'EU') {
+            my $addr = $1;
+            foreach my $c (keys %CO) {
+               if ($addr =~ /$c/i) {
+                  print STDERR "Found $c in address \"$addr\".\n";
+                  $t[1] = $CO{$c};
+                  last;
+               }
+            }
+         }
+      }
+      if ($t[1] eq 'EU') {
+         print STDERR "Attempting Capital City lookup from address lines.\n";
+         while ($w =~ /^address:\s*(.*)$/mg && $t[1] eq 'EU') {
+            my $addr = $1;
+            foreach my $c (keys %CC) {
+               if ($addr =~ /$c/i) {
+                  print STDERR "Found $c in address \"$addr\".\n";
+                  $t[1] = $CC{$c};
+                  last;
+               }
+            }
+         }
+      }
+      if ($t[1] eq 'EU') {
+         print STDERR "COUNTRY NOT FOUND. I give up, I'm just a dumb script.\n";
       }
       print join('|',@t)."\n";
    } else {
